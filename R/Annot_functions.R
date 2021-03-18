0f35b0cfa5d1447f800768445091ace0eb88e255
Türei Dénes <turei.denes@gmail.com>
2021-03-18 17:42:51 +0100

    added some roxygen docstrings

--- R/Annot_functions.R
+++ R/Annot_functions.R
@@ -1,43 +1,94 @@
-### Annotation datasets auxiliar functions 
+### Annotation datasets auxiliary functions
 
-# Aggregate annotation datasets
-aggregate_annot <- function(data_annot,type_annot){
-  if(type_annot == "GO"){
-    data_aggregated <- data_annot %>%
-      dplyr::group_by(GO_ID) %>%
-      dplyr::mutate(Gene_Symbol = paste0(unique(Gene_Symbol),collapse = ",")) %>%
-      ungroup()
-  } else if(type_annot == "HPO"){
-    data_aggregated <- data_annot %>%
-      dplyr::group_by(HPO_ID,HPO_Name) %>%
-      dplyr::mutate(Gene_Symbol = paste0(unique(Gene_Symbol),collapse = ",")) %>%
-      ungroup() 
-  } else {
-    stop("Only possible to aggregate GO or HPO datasets.")
-  }
-  data_aggregated$nr_gene <- count.fields(textConnection(data_aggregated$Gene_Symbol),sep =",")
-  return(data_aggregated)
+#' Aggregate annotation datasets
+#'
+#' @param data_annot ???
+#' @parma type_annot ???
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr group_by mutate ungroup
+#' @importFrom utils count.fields
+#' @export
+aggregate_annot <- function(data_annot, type_annot){
+
+    if(type_annot == "GO"){
+        data_aggregated <- data_annot %>%
+        dplyr::group_by(GO_ID) %>%
+        dplyr::mutate(
+            Gene_Symbol = paste0(unique(Gene_Symbol), collapse = ",")
+        ) %>%
+        ungroup()
+    } else if(type_annot == "HPO"){
+        data_aggregated <- data_annot %>%
+        dplyr::group_by(HPO_ID,HPO_Name) %>%
+        dplyr::mutate(
+            Gene_Symbol = paste0(unique(Gene_Symbol), collapse = ",")
+        ) %>%
+        ungroup()
+    } else {
+        stop("Only possible to aggregate GO or HPO datasets.")
+    }
+    data_aggregated$nr_gene <- count.fields(
+        textConnection(data_aggregated$Gene_Symbol),
+        sep =","
+    )
+
+    return(data_aggregated)
 }
 
-# Number of total genes in each dataset
+
+#' Number of total genes in each dataset
+#'
+#' @param data_annot ???
+#'
+#' @export
 nr_genes <- function(data_annot){
-  length(!is.na(unique(data_annot$Gene_Symbol)))
+
+    length(!is.na(unique(data_annot$Gene_Symbol)))
+
 }
 
-# Filter annotation datasets using network object
-filter_annot_with_network <- function(data_annot,graph_op){
-  genes_op <- vertex_attr(graph_op)$Gene_Symbol
-  data_annot_filter <- data_annot %>%
-    filter(Gene_Symbol %in% genes_op) %>%
-    distinct()
-  return(data_annot_filter)
+
+#' Filter annotation datasets using network object
+#'
+#' @param data_annot ???
+#' @param graph_op ???
+#'
+#' @importFrom igraph vertex_attr
+#' @importFrom magrittr %>%
+#' @importFrom dplyr filter distinct
+#' @export
+filter_annot_with_network <- function(data_annot, graph_op){
+
+    genes_op <- vertex_attr(graph_op)$Gene_Symbol
+    data_annot_filter <- data_annot %>%
+        filter(Gene_Symbol %in% genes_op) %>%
+        distinct()
+
+    return(data_annot_filter)
+
 }
 
-# Functional similarity between two genes in annotation database
-functional_annot <- function(data_aggregated,nr_genes,gene_i,gene_j){
-  idx_i <- grepl(gene_i,data_aggregated$Gene_Symbol)
-  idx_j <- grepl(gene_j,data_aggregated$Gene_Symbol)
-  data_i_j <- data_aggregated[idx_i & idx_j,]$nr_gene
-  functional_i_j <- ifelse(length(data_i_j)!=0,sum((-2)*log(data_i_j/nr_genes)),0)
-  return(functional_i_j)
+
+#' Functional similarity between two genes in annotation database
+#'
+#' @param data_aggregated ???
+#' @param nr_genes ???
+#' @param gene_i ???
+#' @param gene_j ???
+#'
+#' @export
+functional_annot <- function(data_aggregated, nr_genes, gene_i, gene_j){
+
+    idx_i <- grepl(gene_i,data_aggregated$Gene_Symbol)
+    idx_j <- grepl(gene_j,data_aggregated$Gene_Symbol)
+    data_i_j <- data_aggregated[idx_i & idx_j,]$nr_gene
+    functional_i_j <- ifelse(
+        length(data_i_j) != 0,
+        sum(-2 * log(data_i_j / nr_genes)),
+        0
+    )
+
+    return(functional_i_j)
+
 }
--- R/WPPI_app.R
+++ R/WPPI_app.R
@@ -1,44 +1,52 @@
-# General function to run all auxiliar functions in WPPI_functions and Annot_functions
+#' Weight protein-protein interactions
+#'
+#' General function to run all auxiliar functions in WPPI_functions and
+#' Annot_functions
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr filter
+#' @importFrom igraph vertex_attr E V
+#' @export
+#'
+#' @return Data frame with protein-protein interactions of the genes of
+#'      interest scored in the context of the ontology terms of interest.
+score_candidate_genes_from_PPI <- function(
+    genes_interest,
+    HPO_interest,
+    percentage_output_genes,
+    graph_order
+){
 
-source("WPPI_packages.R")
-source("WPPI_data.R")
-source("PPI_functions.R")
-source("Annot_functions.R")
-
-score_candidate_genes_from_PPI <- function(genes_interest,
-                                           HPO_interest,
-                                           percentage_output_genes,
-                                           graph_order){
-  graph_op <- graph_from_op(Omnipath.human.data)
-  edges_op <- E(graph_op)
-  vertices_op <- V(graph_op)
-  if(missing(genes_interest)){
-    genes_interest <- vertex_attr(graph_op)$Gene_Symbol
-  }
-  if(missing(HPO_interest)){
-    HPO_data <- HPO.data %>%
-      filter(HPO_Name %in% HPO_interest)
-  } else {HPO_data <- HPO.data}
-  if(missing(percentage_output_genes)){
-    percentage_output_genes <- 100 # default value
-  }
-  if(missing(graph_order)){
-    graph_order <- 1 # set as default to use the first order neighbors of the graph
-  }
-  sub_graph <- subgraph_op(graph_op,genes_interest,graph_order)
-  GO_data_sub <- filter_annot_with_network(GO.data,sub_graph)
-  HPO_data_sub <- filter_annot_with_network(HPO_data,sub_graph)
-  neighbors_sub <- common_neighbors(sub_graph)
-  weighted_adj_sub <- weighted_adj(sub_graph,
-                               neighbors_sub,
-                               GO_data_sub,
-                               HPO_data_sub,
-                               nr_genes(GO_data_sub),
-                               nr_genes(HPO_data_sub))
-  random_walk_sub <- random_walk(weighted_adj_sub)
-  genes_ranked_sub <- prioritization_genes(sub_graph,
-                                           random_walk_sub,
-                                           genes_interest,
-                                           percentage_genes_ranked = percentage_output_genes)
-  return(genes_ranked_sub)
+    graph_op <- graph_from_op(Omnipath.human.data)
+    edges_op <- E(graph_op)
+    vertices_op <- V(graph_op)
+    if(missing(genes_interest)){
+        genes_interest <- vertex_attr(graph_op)$Gene_Symbol
+    }
+    if(missing(HPO_interest)){
+        HPO_data <- HPO.data %>%
+        filter(HPO_Name %in% HPO_interest)
+    } else {HPO_data <- HPO.data}
+    if(missing(percentage_output_genes)){
+        percentage_output_genes <- 100 # default value
+    }
+    if(missing(graph_order)){
+        graph_order <- 1 # set as default to use the first order neighbors of the graph
+    }
+    sub_graph <- subgraph_op(graph_op,genes_interest,graph_order)
+    GO_data_sub <- filter_annot_with_network(GO.data,sub_graph)
+    HPO_data_sub <- filter_annot_with_network(HPO_data,sub_graph)
+    neighbors_sub <- common_neighbors(sub_graph)
+    weighted_adj_sub <- weighted_adj(sub_graph,
+                                neighbors_sub,
+                                GO_data_sub,
+                                HPO_data_sub,
+                                nr_genes(GO_data_sub),
+                                nr_genes(HPO_data_sub))
+    random_walk_sub <- random_walk(weighted_adj_sub)
+    genes_ranked_sub <- prioritization_genes(sub_graph,
+                                            random_walk_sub,
+                                            genes_interest,
+                                            percentage_genes_ranked = percentage_output_genes)
+    return(genes_ranked_sub)
 }
